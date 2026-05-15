5a19a3be664728766817bf29cfd2ac0bc913ae45
sedin-sivasakthii <siva.sakthii@sedintechnologies.com>
2026-05-15 15:24:03 +0530

    feat: doctor details mock page created

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DoctorDetails } from './features/doctors/doctor-details/doctor-details';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DoctorDetails],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
